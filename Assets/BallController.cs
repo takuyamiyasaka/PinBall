eabfbad232eac3b763acdfbfc912ed77565f5a40
takuyamiyasaka <larsalex8@gmail.com>
2018-10-19 19:34:25 +0900

    PInBall課題
    PinBallのファイルです

---
+++ Assets/BallController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    private float visiblePosZ = -6.5f;
+    private GameObject gameoverText;
+    GameObject realpoint;
+
+   
+
+    // Use this for initialization
+    void Start()
+    {
+        this.gameoverText = GameObject.Find("GameOverText");
+        realpoint = GameObject.Find("Point");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            this.gameoverText.GetComponent<Text>().text = "GameOver Text";
+        }
+        if (point >= 0)
+        {
+            realpoint.GetComponent<Text>().text = point + "Point";
+        }
+        }
+        
+        
+
+    
+    int point = 0;
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "LargeStarTag")
+        {
+            point += 10;
+        }
+        else if (collision.gameObject.tag == "SmallStarTag")
+        {
+            point += 1;
+        }
+        else if (collision.gameObject.tag == "LargeColoudTag")
+        {
+            point += 20;
+        }
+        
+        else if (collision.gameObject.tag == "SmallColoudTag")
+        {
+            point += 15;
+        }
+        Debug.Log(point);
+    }
+}
+
+
---
+++ Assets/BrightnessRegulator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BrightnessRegulator : MonoBehaviour
+{
+    //materialを入れる
+    Material myMaterial;
+    private float minEmission = 0.3f;
+    private float magEmission = 2.0f;
+    private int degree = 0;
+    private int speed = 10;
+    Color defaultColor = Color.white;
+    private int realpoint = 0;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (tag == "SmallStarTag")
+        {
+            this.defaultColor = Color.white;
+            this.realpoint = 5;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.defaultColor = Color.yellow;
+            this.realpoint = 15;
+        }
+        else if (tag == "SmallClouTag" || tag == "LargeCloudTag")
+        {
+            this.defaultColor = Color.cyan;
+            this.realpoint = 20;
+
+        }
+        this.myMaterial = GetComponent<Renderer>().material;
+        myMaterial.SetColor("_EmissionColor", this.defaultColor * minEmission);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.degree >= 0)
+        {
+            Color emissionColor = this.defaultColor * (this.minEmission + Mathf.Sin(this.degree * Mathf.Deg2Rad) * this.magEmission);
+            myMaterial.SetColor("_EmissionColor", emissionColor);
+            this.degree -= this.speed;
+        }
+
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        this.degree = 180;
+    }
+}
+
+
+
